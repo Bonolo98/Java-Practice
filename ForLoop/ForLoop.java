cc22d4e78643c334fdff9ca595f50b6b592415f7
bonolo98 <bonolosolomon9846@gmail.com>
2026-07-05 13:22:06 +0200

    adding forloops

---
+++ ForLoop/ForLoop.java
@@ -0,0 +1,16 @@
+package ForLoop;
+
+public class ForLoop {
+    public static void main(String a[]) {
+        // int x;
+
+        // for(x = 0; x <= 10; x++){
+        // System.out.println("This is x value: " + x);
+        // }
+
+        for (int x = 10; x > 0; x--) {
+            System.out.println("This is x value: " + x);
+        }
+    }
+
+}
