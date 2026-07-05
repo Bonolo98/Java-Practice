dfa37d609f56e5f949179c2d3d09dadbd084a62f
bonolo98 <bonolosolomon9846@gmail.com>
2026-07-05 13:28:05 +0200

    update forloops

--- ForLoop/ForLoop.java
+++ ForLoop/ForLoop.java
@@ -2,15 +2,19 @@ package ForLoop;
 
 public class ForLoop {
     public static void main(String a[]) {
-        // int x;
+        int x;
 
-        // for(x = 0; x <= 10; x++){
-        // System.out.println("This is x value: " + x);
-        // }
-
-        for (int x = 10; x > 0; x--) {
+        for (x = 0; x <= 10; x++) {
             System.out.println("This is x value: " + x);
         }
+
+        for (x = 0; x <= 10; x += 2) {
+            System.out.println("This is x value incremented by 2: " + x);
+        }
+
+        // for (int x = 10; x > 0; x--) {
+        // System.out.println("This is x value: " + x);
+        // }
     }
 
 }
