edb68bc156bdc0b3cdbd130d9e612d3641fa63e6
bonolo98 <bonolosolomon9846@gmail.com>
2026-07-18 20:33:29 +0200

    multi-dimensional arrays practice

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -14,13 +14,27 @@ public class Arrays {
         // int arrLength = arr.length;
 
         // for (int i = 0; i < arrLength; i++) {
-        //     System.out.println(arr[i]);
+        // System.out.println(arr[i]);
         // }
-
 
         // Multi-dimensional Arrays
 
-        int[] 
+        int[][] nums = new int[4][5];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                // System.out.println(nums[i][j] = (int) (Math.random() * 100));
+                nums[i][j] = (int) (Math.random() * 100);
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 
 }
