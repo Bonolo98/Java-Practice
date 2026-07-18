b7518ede9803c8abde44e384cc7f4fe3598c8e52
bonolo98 <bonolosolomon9846@gmail.com>
2026-07-18 20:22:53 +0200

    arrays practice

---
+++ Arrays/Arrays.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        // One Dimensional Arrays
+        // int[] arr = new int[5];
+        // arr[0] = 4;
+        // arr[1] = 2;
+        // arr[2] = 7;
+        // arr[3] = 0;
+        // arr[4] = 3;
+
+        // int arrLength = arr.length;
+
+        // for (int i = 0; i < arrLength; i++) {
+        //     System.out.println(arr[i]);
+        // }
+
+
+        // Multi-dimensional Arrays
+
+        int[] 
+    }
+
+}
